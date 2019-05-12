0b32797dbcf482a60e8ed36c8926fec0cd364d34
sconosci1992 <44251821+sconosci1992@users.noreply.github.com>
2019-05-12 01:17:15 -0500

    Add files via upload

---
+++ port.js
@@ -0,0 +1,17 @@
+alert('hovered');
+
+let tv = document.querySelector('#TV');
+
+let dock = document.querySelector('#dock');
+
+let sake = document.querySelector('#sake');
+
+let vintage = document.querySelector('#vintage');
+
+let dockside = document.querySelector('#dockside');
+
+let saketini = document.querySelector('#dockside');
+
+vintage.addEventListener("mouseover", function(){
+	alert("hovered");
+});
