eaf6662206c7f76d6a8d41c31d109bded1a75fe9
sconosci1992 <44251821+sconosci1992@users.noreply.github.com>
2019-05-23 22:59:29 -0500

    Add files via upload
    Redesign and update to projects

--- port.js
+++ port.js
@@ -1,5 +1,3 @@
-alert('hovered');
-
 let tv = document.querySelector('#TV');
 
 let dock = document.querySelector('#dock');
@@ -10,8 +8,36 @@ let vintage = document.querySelector('#vintage');
 
 let dockside = document.querySelector('#dockside');
 
-let saketini = document.querySelector('#dockside');
+let saketini = document.querySelector('#saketini');
 
 vintage.addEventListener("mouseover", function(){
-	alert("hovered");
+	tv.style.zoom = 1;
 });
+
+vintage.addEventListener("mouseout", function(){
+	tv.style.zoom = 1.3;
+});
+
+dockside.addEventListener("mouseover", function(){
+	dock.style.zoom = 1;
+});
+
+dockside.addEventListener("mouseout", function(){
+	dock.style.zoom = 1.3;
+});
+
+saketini.addEventListener("mouseover", function(){
+	sake.style.zoom = 1;
+});
+
+saketini.addEventListener("mouseout", function(){
+	sake.style.zoom = 1.3;
+});
+
+function zoomIn(){
+	this.style.zoom = 1.3;
+}
+
+function zoomOut(){
+	this.style.zoom = 1;
+}
